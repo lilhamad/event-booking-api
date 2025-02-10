1d70a9edb6d1ca95fe68929f3253ea2d936848dd
Ahmed Adewale <lilhamad8@gmail.com>
2025-02-10 21:35:51 +0100

    server cleanup

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ const colors = require('colors');
 require('dotenv').config();
 const test = require('dotenv').config()
 console.log("***test", test)
-const DBConnect = require('./utils/dbConnect');
-const DbConfig = require('./config/config.db');
 
 process.on('uncaughtException', (error) => {
   // using uncaughtException event
@@ -16,20 +14,13 @@ process.on('uncaughtException', (error) => {
 
 const app = require('./app');
 
-// database connection
-//DBConnect();
-//DbConfig();
 // server
 const port = process.env.PORT || 6000;
 const server = app.listen(port, () => {
   console.log(`App is running on port ${port}`.yellow.bold);
 });
 
-// handle Globaly  the unhandle Rejection Error which is  outside the express
-// e.g database connection
 process.on('unhandledRejection', (error) => {
-  // it uses unhandledRejection event
-  // using unhandledRejection event
   console.log(' Unhandled Rejection => shutting down..... ');
   console.log(error.name, error.message);
   server.close(() => {
